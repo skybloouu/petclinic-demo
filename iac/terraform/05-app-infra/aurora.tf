ae6deb623f473551bf3eed330858b9594e78042f
Aakash Dabrase <rex.aakash@gmail.com>
2025-09-05 00:08:46 +0530

    removed local profile reference

--- iac/terraform/05-app-infra/aurora.tf
+++ iac/terraform/05-app-infra/aurora.tf
@@ -10,10 +10,10 @@ locals {
 data "terraform_remote_state" "network" {
   backend = "s3"
   config = {
-    bucket  = "stackgen-terraform-state"
-    key     = "env/00-network/terraform.tfstate"
-    region  = "ap-south-1"
-    profile = "personal"
+    bucket = "stackgen-terraform-state"
+    key    = "env/00-network/terraform.tfstate"
+    region = "ap-south-1"
+    # profile = "personal"
   }
 }
 
